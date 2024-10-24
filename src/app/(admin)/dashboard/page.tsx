295427c23c78221ce3614310c058ca61a61193e0
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-10-24 20:49:02 +0700

    fitur middleware

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div className="w-full h-96 bg-gray-300 rounded-[12px] flex items-center justify-center">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const isLogin = false;
+  const isLogin = true;
   if (!isLogin) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
